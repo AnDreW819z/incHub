0790ae2bcffb8881fdd4318acb4623fa95a0b79a
kristina_minina <kristina_minina_2002@yandex.ru>
2023-04-29 03:13:09 +0300

    interfaces+rep

---
+++ incHub/Interfaces/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using incHub.Dto;
+using incHub.Models;
+
+namespace incHub.Interfaces
+{
+    public interface IUserRepository
+    {
+        ICollection<User> GetUsers();
+        User GetUser(int userId);
+        ICollection<Project> GetUserOfProjects(int projectId);
+        ICollection<Skill> GetUserBySkill(int skillId);
+        bool UserExists(int userId);
+        bool CreateUser(User user);
+        bool UpdateUser(User user);
+        bool DeleteUser(User user);
+        bool Save();
+    }
+}
---
+++ incHub/Repository/UserRepository.cs
@@ -0,0 +1,67 @@
+﻿using incHub.Data;
+using incHub.Dto;
+using incHub.Interfaces;
+using incHub.Models;
+
+namespace incHub.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DataContext _context;
+
+        public UserRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public bool CreateUser(User user)
+        {
+            _context.Add(user);
+            return Save();
+        }
+
+        public bool DeleteOwner(User user)
+        {
+            _context.Remove(user);
+            return Save();
+        }
+
+        public User GetUser(int userId)
+        {
+            return _context.Users.Where(o => o.Id == userId).FirstOrDefault();
+        }
+
+        public ICollection<User> GetUserOfProject(int projectId)
+        {
+            return _context.Projects.Where(p => p.User.Id == projectId).Select(o => o.User).ToList();
+        }
+
+        public ICollection<User> GetUsers()
+        {
+            return _context.Users.ToList();
+        }
+
+        public ICollection<User> GetUserBySkill(int skillId)
+        {
+            return _context.UserSkills.Where(p => p.Skill.Id == skillId).Select(p => p.User).ToList();
+        }
+
+        public bool UserExists(int userId)
+        {
+            return _context.Users.Any(o => o.Id == userId);
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+
+        public bool UpdateUser(User user)
+        {
+            _context.Update(user);
+            return Save();
+        }
+
+    }
+}
