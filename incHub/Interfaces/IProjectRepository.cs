29fa6440bd2af942c8f7f42db6e789522a4cf747
kristina_minina <kristina_minina_2002@yandex.ru>
2023-04-29 06:39:37 +0300

    сохранение

---
+++ incHub/Interfaces/IProjectRepository.cs
@@ -0,0 +1,17 @@
+﻿using incHub.Dto;
+using incHub.Models;
+
+namespace incHub.Interfaces
+{
+    public interface IProjectRepository
+    {
+        ICollection<Project> GetProjects();
+        Project GetProject(int userId);
+        User GetUserOfProject(int projectId);
+        bool ProjectExists(int projectId);
+        bool CreateProject(Project project);
+        bool UpdateProject(Project project);
+        bool DeleteProject(Project project);
+        bool Save();
+    }
+}
---
+++ incHub/Repository/ProjectRepository.cs
@@ -0,0 +1,64 @@
+﻿using incHub.Data;
+using incHub.Dto;
+using incHub.Interfaces;
+using incHub.Models;
+
+namespace incHub.Repository
+{
+    public class ProjectRepository : IProjectRepository
+    {
+        private readonly DataContext _context;
+
+        public ProjectRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public bool CreateProject(Project project)
+        {
+            _context.Add(project);
+            return Save();
+        }
+
+        public bool DeleteProject(Project project)
+        {
+            _context.Remove(project);
+            return Save();
+        }
+
+        public Project GetProject(int projectId)
+        {
+            return _context.Projects.Where(o => o.Id == projectId).FirstOrDefault();
+        }
+
+        public User GetUserOfProject(int projectId)
+        {
+            return _context.Projects.Where(o => o.Id == projectId).FirstOrDefault().User;
+        }
+
+        public ICollection<Project> GetProjects()
+        {
+            return _context.Projects.ToList();
+        }
+
+       
+
+        public bool ProjectExists(int projectId)
+        {
+            return _context.Projects.Any(o => o.Id == projectId);
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+
+        public bool UpdateProject(Project project)
+        {
+            _context.Update(project);
+            return Save();
+        }
+
+    }
+}
