a02d4885d589810724b813dee0c43bd62a09af03
kristina_minina <kristina_minina_2002@yandex.ru>
2023-04-29 07:08:02 +0300

    project controller

---
+++ incHub/Controllers/ProjectController.cs
@@ -0,0 +1,141 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using incHub.Dto;
+using incHub.Interfaces;
+using incHub.Models;
+
+namespace incHub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectController : Controller
+    {
+        private readonly IProjectRepository _projectRepository;
+        private readonly IMapper _mapper;
+
+        public ProjectController(IProjectRepository projectRepository,
+            IMapper mapper)
+        {
+            _projectRepository = projectRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Project>))]
+        public IActionResult GetProjects()
+        {
+            var projects = _mapper.Map<List<ProjectDto>>(_projectRepository.GetProjects());
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(projects);
+        }
+
+        [HttpGet("{projectId}/user")]
+        [ProducesResponseType(200, Type = typeof(Project))]
+        [ProducesResponseType(400)]
+        public IActionResult GetUserOfProject(int projectId)
+        {
+            if (!_projectRepository.ProjectExists(projectId))
+            {
+                return NotFound();
+            }
+
+            var user = _mapper.Map<List<UserDto>>(
+                _projectRepository.GetUserOfProject(projectId));
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(user);
+        }
+
+        
+
+        [HttpPost]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        public IActionResult CreateProject( [FromBody] ProjectDto projectCreate)
+        {
+            if (projectCreate == null)
+                return BadRequest(ModelState);
+
+            var projects = _projectRepository.GetProjects()
+                .Where(c => c.Name.Trim().ToUpper() == projectCreate.Name.TrimEnd().ToUpper())
+                .FirstOrDefault();
+
+            if (projects != null)
+            {
+                ModelState.AddModelError("", "Project already exists");
+                return StatusCode(422, ModelState);
+            }
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var projectMap = _mapper.Map<Project>(projectCreate);
+
+            if (!_projectRepository.CreateProject(projectMap))
+            {
+                ModelState.AddModelError("", "Something went wrong while savin");
+                return StatusCode(500, ModelState);
+            }
+
+            return Ok("Successfully created");
+        }
+
+        [HttpPut("{projectId}")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        public IActionResult UpdateProject(int projectId, [FromBody] ProjectDto updatedProject)
+        {
+            if (updatedProject == null)
+                return BadRequest(ModelState);
+
+            if (projectId != updatedProject.Id)
+                return BadRequest(ModelState);
+
+            if (!_projectRepository.ProjectExists(projectId))
+                return NotFound();
+
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var projectMap = _mapper.Map<Project>(updatedProject);
+
+            if (!_projectRepository.UpdateProject(projectMap))
+            {
+                ModelState.AddModelError("", "Something went wrong updating project");
+                return StatusCode(500, ModelState);
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{projectId}")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        public IActionResult DeleteProject(int projectId)
+        {
+            if (!_projectRepository.ProjectExists(projectId))
+            {
+                return NotFound();
+            }
+
+            var projectToDelete = _projectRepository.GetProject(projectId);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if (!_projectRepository.DeleteProject(projectToDelete))
+            {
+                ModelState.AddModelError("", "Something went wrong deleting project");
+            }
+
+            return NoContent();
+        }
+    }
+}
